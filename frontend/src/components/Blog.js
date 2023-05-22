3911c08dc36425eca77b0772772c5ebbfa3d85fa
Dilmi <duudi0396@gmail.com>
2023-05-22 14:07:01 +0530

    practise project

---
+++ frontend/src/components/Blog.js
@@ -0,0 +1,103 @@
+import {
+  Avatar,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import ModeEditOutlineIcon from "@mui/icons-material/ModeEditOutline";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useStyles } from "./utils";
+const Blog = ({ title, description, imageURL, userName, isUser, id }) => {
+  const classes = useStyles();
+  const navigate = useNavigate();
+  const handleEdit = () => {
+    navigate(`/myBlogs/${id}`);
+  };
+  const deleteRequest = async () => {
+    const res = await axios
+      .delete(`http://localhost:5000/api/blog/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  const handleDelete = () => {
+    deleteRequest()
+      .then(() => navigate("/"))
+      .then(() => navigate("/blogs"));
+  };
+  return (
+    <div>
+      {" "}
+      <Card
+        sx={{
+          width: "40%",
+          margin: "auto",
+          mt: 2,
+          padding: 2,
+          boxShadow: "5px 5px 10px #ccc",
+          ":hover": {
+            boxShadow: "10px 10px 20px #ccc",
+          },
+        }}
+      >
+        {isUser && (
+          <Box display="flex">
+            <IconButton onClick={handleEdit} sx={{ marginLeft: "auto" }}>
+              <ModeEditOutlineIcon color="warning" />
+            </IconButton>
+            <IconButton onClick={handleDelete}>
+              <DeleteForeverIcon color="error" />
+            </IconButton>
+          </Box>
+        )}
+        <CardHeader
+          avatar={
+            <Avatar
+              className={classes.font}
+              sx={{ bgcolor: "red" }}
+              aria-label="recipe"
+            >
+              {userName ? userName.charAt(0) : ""}
+            </Avatar>
+          }
+          title={title}
+        />
+        <CardMedia
+          component="img"
+          height="194"
+          image={imageURL}
+          alt="Image"
+        />
+
+        <CardContent>
+          <hr />
+          <br />
+          <Typography
+            className={classes.font}
+            variant="body2"
+            color="text.secondary"
+          >
+            <b>{"Description"}</b> {": "} {description}
+          </Typography>
+        </CardContent>
+        <center><Button
+              variant="contained"
+              sx={{ margin: 1, borderRadius: 10 }}
+              color="warning"
+            >
+              Book Now
+            </Button></center>
+      </Card>
+    </div>
+  );
+};
+
+export default Blog;
